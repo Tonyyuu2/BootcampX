d898c2c696f388ffcb2a7e4d0fee2215659d12e8
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-04-04 23:56:03 +0000

    add more queries with JOIN

---
+++ 2_queries_joins/1_student_total_assignment_duration.sql
@@ -0,0 +1,1 @@
+SELECT sum(assignment_submissions.duration) as total_duration FROM assignment_submissions JOIN student ON student.id = student_id WHERE student.name = 'Ibrahim Schimmel';
---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,11 @@
+SELECT sum(assignment_submissions.duration) as total_duration 
+FROM assignment_submissions 
+JOIN student ON student.id = student_id 
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
+
+
+
+
+
+-- JOIN cohorts ON cohorts.start_date = student.start_date WHERE cohorts.start_date = 'FEB12';
